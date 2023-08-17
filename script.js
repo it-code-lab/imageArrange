5f128d79f3d7a0935f8a7a64a2b19ed1c1b5e305
Numero Uno <numerounodeveloper@gmail.com>
2023-08-17 08:31:43 -0400

    fix

---
+++ script.js
@@ -0,0 +1,40 @@
+function showImg(){
+
+    let containerCnt = document.getElementById("containerCnt").value;
+    let containerBorder = document.getElementById("containerBorder").value;
+    let imgCnt = document.getElementById("imgCnt").value;
+    let lastCntImgCnt = document.getElementById("lastCntImgCnt").value;
+    let imgNm = document.getElementById("imgNm").value;
+    let imgWidth = document.getElementById("imgWidth").value;
+
+    //let imageElement = '<img src="img/' + imgNm + '" />';
+
+    let imageElement = '<img style = "width: ' + imgWidth + 'px " src="' + imgNm + '" />';
+
+    let newHTML = '';
+
+    for (let index = 0; index < containerCnt - 1; index++) {
+        newHTML = newHTML + '<div class="imgContainer" style="display: flex; flex-wrap: wrap; border:' + containerBorder + ' ">';
+
+        for (let j = 0; j < imgCnt; j++) {
+            newHTML = newHTML + imageElement ;   
+        }
+
+        newHTML = newHTML + '</div>';
+
+    }
+
+    newHTML = newHTML + '<div class="imgContainer" style="display: flex; flex-wrap: wrap; border:' + containerBorder + ' ">';
+
+    for (let j = 0; j < lastCntImgCnt; j++) {
+        newHTML = newHTML + imageElement ;   
+    }
+
+    newHTML = newHTML + '</div>';
+
+    document.getElementById("outerContainer").innerHTML = newHTML;
+}
+
+function copyOuterHTML(){
+    navigator.clipboard.writeText(document.getElementById("copyContainerHTML").innerHTML);
+}
